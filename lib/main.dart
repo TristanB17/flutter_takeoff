230345b9789dfcfe5a0ec8e99a0571ab9d58a8f4
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2019-01-12 00:07:51 -0700

    Bg color and floating action button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
-
 void main(){
+  var barColor = const Color(0xFFDF3B18);
+  var bgColor = const Color(0xFFF3DCD7);
   var app = MaterialApp(
     home: Scaffold(
-
-    ), 
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: barColor,
+        onPressed:((){}),
+        child: Icon(Icons.public),
+      ),
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        title: Text('WePhrase'),
+        backgroundColor: barColor,
+      ),
+    ),
   );
-  
+
   runApp(app);
 }
